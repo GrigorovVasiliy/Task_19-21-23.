c6434170ce1e370f8c383138d1e95ec024315879
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-10 00:19:57 +0700

    19

---
+++ task_19/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите 5-ти значное число: ");
+string n = Convert.ToString(Console.ReadLine());
+
+if(n[0]==n[4] && n[1]==n[3])
+System.Console.WriteLine("Число является ПАЛИНДРОМОМ");
+else
+System.Console.WriteLine("Число НЕ является ПАЛИНДРОМОМ");
